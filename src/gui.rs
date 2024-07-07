339417e04b8404e06f086a004a4c3da26651c98b
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-07-07 14:41:46 -0400

    have stage, but actors still need to present papers

--- src/gui.rs
+++ src/gui.rs
@@ -1,17 +1,28 @@
 
+use port_scanner::scan_ports_range;
+
+
 use std::process::Command;
 
 
 pub fn run_main() -> Result<(), Box<dyn std::error::Error>> {
 
+  let mut free_qmp_port_num: u16 = 4000;
+  for open_port in scan_ports_range(4000..9000) {
+    free_qmp_port_num = open_port;
+    break;
+  }
+  println!("Using port {:?} for controlling the VM", free_qmp_port_num);
+
+  let t_free_qmp_port_num = free_qmp_port_num;
   std::thread::spawn(move || {
-    control_vm_t();
+    control_vm_t(t_free_qmp_port_num);
   });
 
   Command::new("qemu-system-x86_64")
       .args([
         "-bios", "/usr/share/edk2-ovmf/x64/OVMF_CODE.fd",
-        "-drive", "format=qcow2,file=/mnt/scratch/vms/enice-win11/WinDev2401Eval.qcow2",
+        // "-drive", "format=qcow2,file=/mnt/scratch/vms/enice-win11/WinDev2401Eval.qcow2",
         "-enable-kvm",
         "-m", "8200M",
         "-cpu", "host",
@@ -22,6 +33,7 @@ pub fn run_main() -> Result<(), Box<dyn std::error::Error>> {
         "-boot", "c",
         "-vga", "virtio",
         "-display", "gtk,gl=on",
+        "-qmp", "tcp:localhost:4444,server,wait=on"
       ])
       .status()?;
 
@@ -29,10 +41,33 @@ pub fn run_main() -> Result<(), Box<dyn std::error::Error>> {
   Ok(())
 }
 
-pub fn control_vm_t() {
+pub fn control_vm_t(free_qmp_port_num: u16) {
+  use qapi::{qga, Qga};
+  let socket_addr = format!("127.0.0.1:{}", free_qmp_port_num);
   loop {
-    println!("Control T");
-    std::thread::sleep(std::time::Duration::from_millis(2400));
+    std::thread::sleep(std::time::Duration::from_millis(250));
+    match std::net::TcpStream::connect(&socket_addr) {
+      Err(e) => {
+        eprintln!("Waiting for QEMU: {:?}", e);
+      }
+      Ok(tcp_socket) => {
+        eprintln!("Found QEMU at {:?}", &socket_addr);
+
+        let mut qga = qapi::Qga::from_stream(&tcp_socket);
+
+        let sync_value = &tcp_socket as *const _ as usize as i32;
+        if let Err(e) = qga.guest_sync(sync_value) {
+          eprintln!("Handshake failed: {:?}", e);
+          continue;
+        }
+
+        let info = qga.execute(&qapi::qga::guest_info { }).unwrap();
+        println!("Guest Agent version: {}", info.version);
+
+
+        break;
+      }
+    }
   }
 }
 
